a35319134d13535348f921f7c15a7b11eef4ddb1
MartinRamov <martin.ramov@yahoo.com>
2020-09-03 00:34:30 +0200

    [no-issue] Update latest changes.

--- src/main/java/com/netcetera/example/application/model/LogItem.java
+++ src/main/java/com/netcetera/example/application/model/LogItem.java
@@ -2,6 +2,7 @@ package com.netcetera.example.application.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 /**
  * Class that holds the parameters used in the log entry.
@@ -10,6 +11,7 @@ import lombok.Data;
 @Builder
 public class LogItem {
 
+  @NonNull
   private String id;
   private String url;
   private String method;
